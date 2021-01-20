c8c07538b473764337fe82ef2aaac4e139776cf5
Reikster <richardo.nykanen@gmail.com>
2021-01-20 14:07:45 +0200

    created an adapter

--- app/src/main/java/com/choicely/csvcompanion/LibraryHomeActivity.java
+++ app/src/main/java/com/choicely/csvcompanion/LibraryHomeActivity.java
@@ -1,24 +1,28 @@
 package com.choicely.csvcompanion;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class LibraryHomeActivity extends AppCompatActivity {
 
     private RecyclerView libraryRecycler;
     private Button newLibraryButton;
+    private LibraryAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library_home);
+        setContentView(R.layout.library_home_activity);
+
+        libraryRecycler = findViewById(R.id.library_home_activity_recycler);
+        libraryRecycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new LibraryAdapter(this);
+
 
     }
-
 }
