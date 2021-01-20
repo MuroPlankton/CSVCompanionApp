065a4f6b8b9cea71028773293b8f5bfbb9b3e7e3
Reikster <richardo.nykanen@gmail.com>
2021-01-20 14:39:11 +0200

    realm gradle stuff

---
+++ app/src/main/java/com/choicely/csvcompanion/app/CSVCompanionApp.java
@@ -0,0 +1,13 @@
+package com.choicely.csvcompanion.app;
+
+import android.app.Application;
+
+public class CSVCompanionApp extends Application {
+
+    private static final String TAG = "CSVCompanionApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
