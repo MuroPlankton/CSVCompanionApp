c55f6566775adef392f739cedfe92c687b495a86
MuroPlankton <73102721+MuroPlankton@users.noreply.github.com>
2021-01-20 15:35:58 +0200

    Minor things EditTranslationActivity
    Small changes to it's xml and started working on the code. pushing this because I need to pull something.

--- app/src/main/java/com/choicely/csvcompanion/EditTranslationActivity.java
+++ app/src/main/java/com/choicely/csvcompanion/EditTranslationActivity.java
@@ -2,15 +2,26 @@ package com.choicely.csvcompanion;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditTranslationActivity extends AppCompatActivity {
 
+    private Button anotherTranslationButton;
+    private EditText translationName, transLationDesc;
+    private EditText androidKey, iosKey, webKey;
+    private Spinner langSpinner;
+    private EditText translationValue;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.edit_translation_activity);
+
+        
     }
 }
