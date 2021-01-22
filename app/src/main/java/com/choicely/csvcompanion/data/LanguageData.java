9d96a8cde257ed95c38e7418916bce831f03b8e3
MuroPlankton <73102721+MuroPlankton@users.noreply.github.com>
2021-01-22 10:45:41 +0200

    Another data rework
    Languages and single translations got their own data classes. Everything that used to be in a list is now in a realmlist. Including the new data classes.

---
+++ app/src/main/java/com/choicely/csvcompanion/data/LanguageData.java
@@ -0,0 +1,23 @@
+package com.choicely.csvcompanion.data;
+
+public class LanguageData {
+
+    private String langKey;
+    private String langName;
+
+    public String getLangKey() {
+        return langKey;
+    }
+
+    public void setLangKey(String langKey) {
+        this.langKey = langKey;
+    }
+
+    public String getLangName() {
+        return langName;
+    }
+
+    public void setLangName(String langName) {
+        this.langName = langName;
+    }
+}
