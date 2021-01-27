df2f7f491068e7cce481e84f9999368d818fc5f1
BC\s1800861 <richardo.nykanen@gmail.com>
2021-01-27 12:18:41 +0200

    added a search feature to home page.

--- app/src/main/java/com/choicely/csvcompanion/home/LibraryHomeActivity.java
+++ app/src/main/java/com/choicely/csvcompanion/home/LibraryHomeActivity.java
@@ -2,7 +2,14 @@ package com.choicely.csvcompanion.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +29,13 @@ public class LibraryHomeActivity extends AppCompatActivity {
 
     private static final String TAG = "LibraryHomeActivity";
 
-    private RecyclerView libraryRecycler;
     private Button newLibraryButton;
+    private ImageButton searchButton;
+    private ImageButton backSpace;
+    private EditText searchField;
+    private TextView heading;
+    private boolean isSearchActive = false;
+    private RecyclerView libraryRecycler;
     private LibraryAdapter adapter;
 
     @Override
@@ -31,12 +43,27 @@ public class LibraryHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.library_home_activity);
 
-        newLibraryButton = findViewById(R.id.activity_library_home_new_library);
+        searchButton = findViewById(R.id.library_home_activity_search_button);
+        searchField = findViewById(R.id.library_home_activity_search_field);
+        backSpace = findViewById(R.id.library_home_activity_backspace);
+        heading = findViewById(R.id.library_home_activity_heading);
 
-        newLibraryButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, LibraryActivity.class);
-            startActivity(intent);
+        searchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateSearchedContent();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
         });
+
+        newLibraryButton = findViewById(R.id.activity_library_home_new_library);
 
         libraryRecycler = findViewById(R.id.library_home_activity_recycler);
         libraryRecycler.setLayoutManager(new LinearLayoutManager(this));
@@ -44,6 +71,35 @@ public class LibraryHomeActivity extends AppCompatActivity {
         libraryRecycler.setAdapter(adapter);
 
         startFireBaseListening();
+    }
+
+    public void onClick(View v) {
+        if (v == searchButton) {
+            isSearchActive = true;
+            searchActivity();
+        } else if (v == backSpace) {
+            Log.d(TAG, "backspace clicked");
+            isSearchActive = false;
+            searchActivity();
+        } else if (v == newLibraryButton) {
+            Intent intent = new Intent(this, LibraryActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void updateSearchedContent() {
+        adapter.clear();
+
+        RealmHelper helper = RealmHelper.getInstance();
+        Realm realm = helper.getRealm();
+
+        RealmResults<LibraryData> libraryNames = realm.where(LibraryData.class).contains("libraryName", searchField.getText().toString()).findAll();
+
+        for (LibraryData libraryName : libraryNames) {
+            adapter.add(libraryName);
+        }
+
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -66,10 +122,22 @@ public class LibraryHomeActivity extends AppCompatActivity {
 
         RealmResults<LibraryData> libraries = realm.where(LibraryData.class).findAll();
 
-        for(LibraryData library : libraries){
+        for (LibraryData library : libraries) {
             adapter.add(library);
         }
 
         adapter.notifyDataSetChanged();
+    }
+
+    private void searchActivity() {
+        if (isSearchActive) {
+            searchField.setVisibility(View.VISIBLE);
+            backSpace.setVisibility(View.VISIBLE);
+            heading.setVisibility(View.INVISIBLE);
+        } else {
+            searchField.setVisibility(View.GONE);
+            backSpace.setVisibility(View.GONE);
+            heading.setVisibility(View.VISIBLE);
+        }
     }
 }
